5f724512197b8076b427f737d4f3cc71be97fd89
CrazyKada <kada@MacBook-Pro-de-Kada.local>
2017-02-21 12:46:31 +0100

    iOS Real Time Chat

---
+++ RealTimeChat/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  RealTimeChat
+//
+//  Created by Kada on 18/12/2016.
+//  Copyright © 2016 Kada. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+
+}
